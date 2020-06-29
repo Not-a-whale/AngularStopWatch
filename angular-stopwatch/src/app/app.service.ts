d145f6dad9c021b33db4a10b545e257f82cad799
sabaoth <sabaoth@ukr.net>
2020-06-29 15:49:03 +0300

    unfortunately almost everything

---
+++ angular-stopwatch/src/app/app.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import { Subject, pipe } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class appService {
+  public hours = new Subject<number>();
+  public minutes = new Subject<number>();
+  public seconds = new Subject<number>();
+
+  getHours(hours: number) {
+    this.hours.next(hours);
+  }
+
+  getMinutes(minutes: number) {
+    this.minutes.next(minutes);
+  }
+
+  getSeconds(seconds: number) {
+    this.seconds.next(seconds);
+  }
+
+  deg = 6;
+  hh = 0;
+  mm = 0;
+  ss = 0;
+  isStarted = false;
+  clickWaitCount = 0;
+  interval = null;
+  /*     this.stopWatch();
+    console.log(this.hh, this.mm, this.ss); */
+
+  stopWatch() {
+    this.ss = this.ss + this.deg;
+    this.getSeconds(this.ss);
+    if (this.ss / (60 * this.deg) === 1) {
+      this.ss = 0;
+      this.mm = this.mm + 6;
+      this.getMinutes(this.mm);
+
+      if (this.mm / (60 * this.deg) === 1) {
+        this.mm = 0;
+        this.hh = this.hh + 6;
+        this.getHours(this.hh);
+      }
+    }
+  }
+  startStopClock() {
+    if (!this.isStarted) {
+      // Start stopwatch (by calling setInterval() function
+      this.interval = setInterval(() => {
+        this.stopWatch();
+      }, 1000);
+      this.isStarted = true;
+    } else {
+      this.ss = 0;
+      this.mm = 0;
+      this.hh = 0;
+      this.getHours(this.hh);
+      this.getMinutes(this.mm);
+      this.getSeconds(this.ss);
+      window.clearInterval(this.interval);
+      this.isStarted = false;
+    }
+  }
+
+  resetClock() {
+    this.ss = 0;
+    this.mm = 0;
+    this.hh = 0;
+    this.getHours(this.hh);
+    this.getMinutes(this.mm);
+    this.getSeconds(this.ss);
+    window.clearInterval(this.interval);
+    this.interval = window.setInterval(() => {
+      this.stopWatch();
+    }, 1000);
+
+    !this.isStarted;
+  }
+
+  waitClock() {
+    this.clickWaitCount++;
+    setTimeout(() => {
+      this.clickWaitCount = 0;
+    }, 300);
+    if (this.clickWaitCount >= 2) {
+      window.clearInterval(this.interval);
+      this.isStarted = false;
+    }
+  }
+}
---
+++ angular-stopwatch/src/app/clock-view/clock-view.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { appService } from '../app.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-clock-view',
+  templateUrl: './clock-view.component.html',
+  styleUrls: ['./clock-view.component.scss'],
+})
+export class ClockViewComponent implements OnInit {
+  // Getting DOM Elements
+  /*   @ViewChild("reset", {static: false}) reset: ElementRef;
+  @ViewChild("closeSharp", {static: false}) closeSharp: ElementRef;
+  @ViewChild("continueButton", {static: false}) continueButton: ElementRef; */
+  @ViewChild('hr', { static: false }) hr: ElementRef;
+  @ViewChild('mn', { static: false }) mn: ElementRef;
+  @ViewChild('sc', { static: false }) sc: ElementRef;
+
+  hoursChangedSub: Subscription;
+  minutesChangedSub: Subscription;
+  secondsChangedSub: Subscription;
+
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+
+  rotate() {
+    this.secondsChangedSub = this.appService.seconds.subscribe((seconds) => {
+      this.seconds = seconds;
+
+      this.sc.nativeElement.style.transform = `rotateZ(${this.seconds}deg)`;
+    });
+    this.minutesChangedSub = this.appService.minutes.subscribe((minutes) => {
+      this.minutes = minutes;
+      this.mn.nativeElement.transform = `rotateZ(${this.minutes}deg)`;
+    });
+    this.hoursChangedSub = this.appService.hours.subscribe((hours) => {
+      this.hr.nativeElement.style.transform = `rotateZ(${this.hours}deg)`;
+    });
+  }
+
+  onReset() {
+    this.appService.resetClock();
+    this.rotate();
+  }
+
+  onStartStop() {
+    this.appService.startStopClock();
+    this.rotate();
+  }
+
+  onWait() {
+    console.log('sosi pisyu');
+  }
+
+  constructor(public appService: appService) {}
+
+  ngOnInit(): void {}
+}
